926dd7c017b7944fde8bbea1dd3a89eac3a878c2
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-17 00:17:37 +0800

    >  compile NEMU 20192160000 mips32 Linux mips32-soc 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:17:37 up  8:26,  1 user,  load average: 0.49, 0.42, 0.43 431e53ec7797b8fb151da10cfbb7337a467b2197

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -70,7 +70,7 @@ make_group(group3_b,
 /* 0xf7 */
 make_group(group3_v,
 	inv, inv, not_rm_v, inv, 
-	inv, imul_rm2a_v, inv, idiv_rm_v)
+	mul_rm_v, imul_rm2a_v, inv, idiv_rm_v)
 
 /* 0xfe */
 make_group(group4,
