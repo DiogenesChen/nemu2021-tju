ff30f4711283cd0b5c4498a1e70a0b0b57ea69de
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-17 00:37:28 +0800

    >  compile NEMU 20192160000 mips32 Linux mips32-soc 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:37:28 up  8:45,  1 user,  load average: 0.65, 0.52, 0.43 88c4ed9f79a373d8730580bd13d4ae065f9bd4dd

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -10,8 +10,9 @@ static void do_execute () {
 	dest >>= count;
 	OPERAND_W(op_dest, dest);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	/* There is no need to update EFLAGS, since no other instructions 
+	 * in PA will test the flags updated by this instruction.
+	 */
 
 	print_asm_template2();
 }
