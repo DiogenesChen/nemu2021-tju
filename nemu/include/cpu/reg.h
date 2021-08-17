4d3d711f55fc53234b468a75344125a53f07b6cd
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-17 15:36:21 +0800

    >  compile NEMU 20192160000 mips32 Linux mips32-soc 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:36:21 up 11:08,  1 user,  load average: 0.11, 0.12, 0.15 cbe2d033e08a3a9eb617c83f27a20c2077dd47

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,19 +15,15 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-     union{
-        union {
+     struct {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
-	} gpr[8];
+     } gpr[8];
 
-	/* Do NOT change the order of the GPRs' definitions. */
+     /* Do NOT change the order of the GPRs' definitions. */
 
-	struct{
-            uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-        };
-     };
+     uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
      swaddr_t eip;
      
