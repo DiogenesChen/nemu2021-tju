1ea20fb0d1c9ba480f187df6a3dfca603b1498f2
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-26 23:48:14 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:48:14 up  9:41,  1 user,  load average: 0.17, 0.17, 0.09 a4826fab84b4407a394edd4793da8a102a87c89

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -147,8 +147,8 @@ uint32_t make_dop(uint32_t lp, uint32_t rp){
     int i, j;
     int dop = lp;
 	int min_priority = 10;
-	printf("here!\n");
 	printf("%d", dop);
+	printf("here!\n");
     for(i = lp; i <= rp; i ++){
 		printf("%d", i);
         if (tokens[i].type == NUMBER || tokens[i].type == HNUMBER || tokens[i].type == REGISTER)
