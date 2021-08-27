6ca56fd1042b0a332b259910ef5a191c0f2e5093
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-27 16:26:34 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:26:34 up 17:02,  1 user,  load average: 0.28, 0.09, 0.03 1a5349849e0cd9c4f783895795d579627ecbafe

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -115,7 +115,6 @@ static int cmd_w(char* args){
     bool suc;
     wp = new_wp();
     printf ("Watchpoint %d: %s\n",wp -> NO, args);
-	printf("%d", expr(args, &suc));
     wp -> val = expr (args,&suc);
     strcpy (wp -> args, args);
     if (!suc) Assert (1,"Wrong expression\n");
