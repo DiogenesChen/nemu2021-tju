efd95dd56026b7dedea4e8456d54279305600593
3020244316-Zhonghao Chen <diogeneschen@126.com>
2021-08-27 19:38:31 +0800

    PA1.3 Set watchpoints, v0.2 changed for WP struct

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -116,7 +116,6 @@ static int cmd_w(char* args){
     wp = new_wp();
     printf ("Watchpoint %d: %s\n",wp -> NO, args);
     wp -> val = expr (args,&suc);
-	printf("%d", wp -> val);
     strcpy (wp -> args, args);
     if (!suc) Assert (1,"Wrong expression\n");
     printf ("Value : %d\n",wp -> val);
