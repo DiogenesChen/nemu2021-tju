0a05d8d46de3603f63838b32f37794b6e9aa389e
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-03 22:46:24 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:46:24 up 14:55,  1 user,  load average: 0.00, 0.01, 0.00 ae9f5507cd51a3e962980765dc38a3fae8467a45

--- nemu/src/cpu/exec/control/jcc/je-template.h
+++ nemu/src/cpu/exec/control/jcc/je-template.h
@@ -3,10 +3,8 @@
 #define instr je
 
 static void do_execute(){
-	DATA_TYPE_S val = op_src->val;
 	print_asm("je %x",cpu.eip + 1 + DATA_BYTE + op_src->val);
-	if(cpu.eflags.ZF == 1)
-	cpu.eip += val;
+	if(cpu.eflags.ZF == 1) cpu.eip += op_src->val;
 }
 
 make_instr_helper(i)
--- nemu/src/cpu/exec/logic/cmp.c
+++ nemu/src/cpu/exec/logic/cmp.c
@@ -12,4 +12,8 @@
 #include "cmp-template.h"
 #undef DATA_BYTE
 
+make_helper_v(cmp_i2a)
 make_helper_v(cmp_i2rm)
+make_helper_v(cmp_si2rm)
+make_helper_v(cmp_r2rm)
+make_helper_v(cmp_rm2r)
