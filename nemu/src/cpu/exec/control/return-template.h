823fa38de223be792c70b8950eff1550e09e9940
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 14:06:43 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:06:43 up  1:45,  1 user,  load average: 0.00, 0.01, 0.00 f3de24cfc0b4b3d4cb34d25f2899cedcd7e5f8f1

--- nemu/src/cpu/exec/control/return-template.h
+++ nemu/src/cpu/exec/control/return-template.h
@@ -1,10 +1,12 @@
 #include "cpu/exec/template-start.h"
 
-make_helper(ret){
-	cpu.eip = MEM_R(reg_l(R_ESP));
-	reg_l(R_ESP) += DATA_BYTE;
+#define instr ret
+
+make_helper(concat(ret_, SUFFIX)){
+	cpu.eip = MEM_R (reg_l (R_ESP));
+	reg_l (R_ESP) += DATA_BYTE;
 	print_asm("ret");
-    return 1;
+	return 1;
 }
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/return.h
+++ nemu/src/cpu/exec/control/return.h
@@ -1,6 +1,8 @@
 #ifndef _RET_H_
 #define _RET_H_
 
-make_helper(ret);
+make_helper(ret_b);
+
+make_helper(ret_v);
 
 #endif
