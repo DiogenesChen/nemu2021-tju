914b0e6d2f5fa4d77a8539dc9c78aa5de9b4fafa
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 14:46:03 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:46:03 up  2:24,  1 user,  load average: 0.18, 0.10, 0.03 4940343a74a395204409bff5439f375eb4bdf314

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -5,7 +5,7 @@
 static void do_execute () {
 	if (DATA_BYTE == 1)op_src->val = (int8_t)op_src->val;
 	reg_l (R_ESP) -= 4;
-	swaddr_write (reg_l (R_ESP) , 4 , op_src->val);
+	swaddr_write (reg_l(R_ESP), 4, op_src->val);
 	print_asm_template1();
 }
 
