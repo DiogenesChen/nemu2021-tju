1f6c6cca5a5419316dd15c631213a9b1665facdc
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 11:44:45 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:44:45 up 4 min,  1 user,  load average: 0.25, 0.36, 0.18 9abe45cc215fc06ec9a4b9a5da3b1a8afb6e959

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -14,13 +14,8 @@ static void do_execute() {
 	print_asm_template2();
 }
 
-make_instr_helper(i2a)
-make_instr_helper(i2rm)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(si2rm)
 #endif
-make_instr_helper(r2rm)
-make_instr_helper(rm2r)
-
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/logic/cmp.c
+++ nemu/src/cpu/exec/logic/cmp.c
@@ -1,9 +1,5 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "cmp-template.h"
-#undef DATA_BYTE
-
 #define DATA_BYTE 2
 #include "cmp-template.h"
 #undef DATA_BYTE
@@ -12,8 +8,4 @@
 #include "cmp-template.h"
 #undef DATA_BYTE
 
-make_helper_v(cmp_i2a)
-make_helper_v(cmp_i2rm)
 make_helper_v(cmp_si2rm)
-make_helper_v(cmp_r2rm)
-make_helper_v(cmp_rm2r)
