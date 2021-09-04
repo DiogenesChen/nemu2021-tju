5f8ca5171b6074c069c47417a254aa9a725bb2b1
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 15:27:05 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:27:05 up  3:05,  1 user,  load average: 0.02, 0.04, 0.00 fb00a0335ad471cb7b5be7b49493b1dbe26025f

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -2,12 +2,15 @@
 
 #define instr call
 
-static void do_execute() {
-    cpu.eip += op_src->val;
-    print_asm_template1();
+make_helper(concat(call_i_, SUFFIX)){
+    int len = concat(decode_i_, SUFFIX) (cpu.eip + 1);
+	reg_l (R_ESP) -= DATA_BYTE;
+	swaddr_write (reg_l (R_ESP), 4, cpu.eip + len);
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("call %x",cpu.eip + 1 + len + displacement);
+	cpu.eip += displacement;
+	return len + 1;
 }
-
-make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
 
