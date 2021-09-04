541def2b46a5d4d72a8186ee79b349acf97d28d3
3020244316-Zhonghao Chen <diogeneschen@126.com>
2021-09-04 15:36:15 +0800

    PA2.1 Sys instruction, v1.1

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -4,9 +4,8 @@
 
 make_helper(concat(call_i_, SUFFIX)){
     int len = concat(decode_i_, SUFFIX) (eip + 1);
-	cpu.esp -= DATA_BYTE;
-	uint32_t temp =  cpu.eip + (len + 1);       
-	swaddr_write(cpu.esp, 4, temp);
+	cpu.esp -= DATA_BYTE;      
+	swaddr_write(cpu.esp, 4, cpu.eip + (len + 1));
     cpu.eip += op_src->val;
 	print_asm("call %x", cpu.eip + 1 + len + op_src->val);
 	return len + 1;
