7454297ff3dc061e3183ff0802ace6aa7b73ca86
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-05 19:46:37 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:46:37 up 16:26,  1 user,  load average: 0.12, 0.07, 0.06 a9c738ed50de409a6225c113d5b0132adaa3a0db

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -4,10 +4,19 @@
 
 make_helper(concat(call_i_, SUFFIX)){
     int len = concat(decode_i_, SUFFIX) (eip + 1);
-	cpu.esp -= DATA_BYTE;      
-	swaddr_write(cpu.esp, 4, cpu.eip + (len + 1));
+	reg_l(R_ESP) -= DATA_BYTE;      
+	swaddr_write(reg_l(R_ESP), 4, cpu.eip + (len + 1));
     cpu.eip += op_src->val;
 	print_asm("call %x", cpu.eip + 1 + len + op_src->val);
+	return len + 1;
+}
+
+make_helper (concat(call_rm_, SUFFIX)){
+	int len = concat(decode_rm_, SUFFIX) (eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	swaddr_write (reg_l(R_ESP), 4, cpu.eip + (len + 1));
+	print_asm("call %x", op_src->val);
+	cpu.eip = op_src->val - len - 1;
 	return len + 1;
 }
 
