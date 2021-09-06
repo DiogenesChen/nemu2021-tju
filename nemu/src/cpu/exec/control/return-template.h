ae12091ef6c0f9218ab272dd39da9932e80c6c6d
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-06 14:53:59 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:53:59 up 20:54,  1 user,  load average: 0.03, 0.08, 0.08 23065dc7180320f3e055367722fb15d813d9968e

--- nemu/src/cpu/exec/control/return-template.h
+++ nemu/src/cpu/exec/control/return-template.h
@@ -1,12 +1,20 @@
 #include "cpu/exec/template-start.h"
 
-#define instr ret
-
-make_helper(concat(ret_, SUFFIX)){
-	cpu.eip = MEM_R(reg_l(R_ESP)) - 1;
-	cpu.esp += DATA_BYTE;
+make_helper(ret){
+	cpu.eip = swaddr_read(reg_l(R_ESP), 4) - 1;
+	cpu.esp += 4;
 	print_asm("ret");
 	return 1;
+}
+
+make_helper(ret_i){
+	uint16_t imm = instr_fetch(eip + 1 , 2);
+	cpu.eip = swaddr_read(cpu.esp,4) - (2 + 1);
+	cpu.esp += 4 + imm;
+
+	print_asm("ret $0x%x", imm);
+
+	return 2 + 1;
 }
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/return.h
+++ nemu/src/cpu/exec/control/return.h
@@ -1,8 +1,8 @@
 #ifndef _RET_H_
 #define _RET_H_
 
-make_helper(ret_b);
+make_helper(ret);
 
-make_helper(ret_v);
+make_helper(ret_i);
 
 #endif
