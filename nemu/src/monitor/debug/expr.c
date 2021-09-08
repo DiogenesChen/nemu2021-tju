be2045a09dbcd54b90ec658aa5aa0a8f45dcb339
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-08 13:53:41 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:53:41 up 1 day, 13:12,  1 user,  load average: 0.03, 0.06, 0.04 eedcf87551d9ba5f35872dde3293585b4307328b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -39,7 +39,7 @@ static struct rule {
     {"\\)", ')', 7},                // braces
     {"\\b0[xX][0-9a-fA-F]{1,31}\\b",HNUMBER,0},        // hex number
     {"\\$[a-zA-Z]{2,3}",REGISTER,0},                  // register
-    {"\\b[a-zA-Z][a-zA-Z_0-9]{1,31}", VAR, 0},    // varibale
+    {"\\[a-zA-Z][a-zA-Z_0-9]*", VAR, 0},    // varibale
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
