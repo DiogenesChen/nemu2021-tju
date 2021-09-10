41a9a9ba6f59ec676064d5072ee6a42101039552
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-10 09:07:46 +0800

    >  compile kernel 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:07:46 up 1 day, 20:30,  1 user,  load average: 0.42, 0.50, 0.27 3e519611313763edca9c880eacc8141a542a2f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,12 +31,12 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
+	panic("Please implement me");
 	for(; true; ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
