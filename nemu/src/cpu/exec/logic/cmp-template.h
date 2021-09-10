33c93649d3768c2c0d7aba41bd9d01c02cd3d2e1
3020244316-Zhonghao Chen <diogeneschen@126.com>
2021-09-10 18:15:45 +0800

    PA2.4 v1.0

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -4,11 +4,9 @@
 
 static void do_execute(){
 	DATA_TYPE result = op_dest->val - op_src->val;
-	
 	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
 	cpu.eflags.CF = result > op_dest->val;
 	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^result));
-
 	print_asm_template2();
 }
 
