6af035428b88b74202c6180a88358afca85602e8
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-29 20:07:23 +0800

    >  compile NEMU 3020244316 faust Linux ubuntu 5.4.0-81-generic #91~18.04.1-Ubuntu SMP Fri Jul 23 13:36:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:07:23 up 1 day, 16:22,  1 user,  load average: 0.11, 0.16, 0.19 3c5368e2f5044b7e3e3e5420bcd67f0fc5b0c17e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -20,28 +20,32 @@ lnaddr_t seg_translate(swaddr_t addr,size_t len,uint8_t sreg_id){
 	}
 }
 
-hwaddr_t page_translate(lnaddr_t addr){
-	if (cpu.cr0.protect_enable == 1 && cpu.cr0.paging == 1){
-		uint32_t dir = addr >> 22;
-		uint32_t page = (addr >> 12) & 0x3ff;
-		uint32_t offset = addr & 0xfff;
-		int i = read_tlb(addr);
-		if (i != -1) return (tlb[i].page_num << 12) + offset;
-		uint32_t dir_start = cpu.cr3.page_directory_base;
-		uint32_t dir_pos = (dir_start << 12) + (dir << 2);
-		Page_Descriptor first_content;
-		first_content.val = hwaddr_read(dir_pos,4);
-		Assert(first_content.p == 1,"Dir Cannot Be Used");
-		uint32_t page_start = first_content.addr;
-		uint32_t page_pos = (page_start << 12) + (page << 2);
-		Page_Descriptor second_content;
-		second_content.val =  hwaddr_read(page_pos,4);
-		Assert(second_content.p == 1,"Page Cannot Be Used!");
-		uint32_t addr_start = second_content.addr;
-		hwaddr_t hwaddr = (addr_start << 12) + offset;
-		write_tlb(addr,hwaddr);
-		return hwaddr;
-	}else return addr;
+hwaddr_t page_translate(lnaddr_t addr) {
+  if (cpu.cr0.protect_enable == 1 && cpu.cr0.paging == 1) {
+    uint32_t dir = addr >> 22;
+    uint32_t page = (addr >> 12) & 0x3ff;
+    uint32_t bias = addr & 0xfff;
+
+    int i = read_tlb(addr);
+    if (i != -1) return (tlb[i].page_num << 12) + bias;
+
+    uint32_t dir_start = cpu.cr3.page_directory_base;
+    uint32_t dir_position = (dir_start << 12) + (dir << 2);
+    Page_Descriptor first_content;
+    first_content.val = hwaddr_read(dir_position, 4);
+    Assert(first_content.p == 1, "Page unavailable");
+    uint32_t page_start = first_content.addr;
+    uint32_t page_pos = (page_start << 12) + (page << 2);
+    Page_Descriptor second_content;
+    second_content.val =  hwaddr_read(page_pos, 4);
+    Assert(second_content.p == 1, "Page unavailable");
+    uint32_t addr_start = second_content.addr;
+    hwaddr_t hwaddr = (addr_start << 12) + bias;
+    write_tlb(addr, hwaddr);
+    return hwaddr;
+  } else {
+    return addr;
+  }
 }
 
 hwaddr_t page_translate_additional(lnaddr_t addr,int* flag){
